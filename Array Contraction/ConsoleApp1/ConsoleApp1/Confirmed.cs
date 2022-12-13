52ed0f7794dabfd220dd77e2e34ef555528f27b3
Cavidismailzadeh <119400508+Cavidismailzadeh@users.noreply.github.com>
2022-12-13 18:43:13 +0400

    Array classwork

---
+++ Array Contraction/ConsoleApp1/ConsoleApp1/Confirmed.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Confirmed
+    {
+        public string name;
+        public string surname;
+        public string address;
+       
+    }
+}
+
+namespace ConsoleApp1
+{
+    internal class Confirmed2
+{
+    public string name;
+    public string surname;
+    public string address;
+
+        public string FullConfirmed2()
+        {
+            return name + "-"+ surname +"-" + address;        }
+
+}
+}
+
+
+
+namespace ConsoleApp1
+{
+    internal class Confirmed3
+    {
+        public string name;
+        public string surname;
+        public string address;
+
+        public string FullConfirmed3()
+        {
+            return $"{name}, {surname}, {address}"; 
+        }
+
+    }
+}
---
+++ Array Contraction/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿//using ConsoleApp1;
+
+//Confirmed confirmed = new Confirmed();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine($"{confirmed.name}-{confirmed.surname}-{confirmed.address}");
+
+
+
+
+//using ConsoleApp1;
+
+//Confirmed2 confirmed = new Confirmed2();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine(confirmed.FullConfirmed2());
+
+
+
+
+//using ConsoleApp1;
+
+//Confirmed3 confirmed = new Confirmed3();
+
+//confirmed.name = "Shaiq";
+//confirmed.surname = "Kazimov";
+//confirmed.address = "Sedmoy";
+
+//Console.WriteLine(confirmed.FullConfirmed3());
+
+
+
+
+//string name1 = "Cavid";
+//string surname1 = "Ismayilzade";
+//int age1 = 22;
+
+//Console.WriteLine($"{name1} {surname1} {age1}");
+
+
+
+//string name2 = "Shaiq";
+//string surname2 = "Kazimov";
+//int age2 = 25;
+
+//Console.WriteLine($"{name2} {surname2} {age2}");
+
+
+
+//string name3 = "Cavid";
+//string surname3 = "Bashirov";
+//int age3 = 29;
+
+//Console.WriteLine($"{name3} {surname3} {age3}");
+
+
+
+//var stu1 = new
+//{
+//    name = "cavid",
+//    surname = "ismayilzade",
+//    age = 22 
+//};
+
+//Console.WriteLine(stu1.name+ "-" +stu1.surname+ "-"+stu1.age);
+
+
+
+
+
+
