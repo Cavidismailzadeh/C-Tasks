f57f9bad139caae005db05c2396235eb6567be5a
Cavidismailzadeh <119400508+Cavidismailzadeh@users.noreply.github.com>
2022-12-14 18:53:38 +0400

    new

---
+++ OOP-Acces Modifys/OOP-AccesModifys/OOP-AccesModifys/StudentInfo.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace OOP_AccesModifys
+//{
+//    internal class StudentInfo
+//    {
+//        public string name;
+//        public string surname;
+//        public int age;
+
+//        public StudentInfo()
+//        {
+//            Console.WriteLine("Empty Info");
+//        }
+//        public StudentInfo(string text)
+//        {
+//            Console.WriteLine(text);
+//        }
+//        public StudentInfo(string m ,int n)
+//        {
+//            Console.WriteLine(n);
+//        }
+//    }
+//}
+
+
+
+
+
+//namespace OOP_AccesModifys
+//{
+//    internal class StudentInfo
+//    {
+//        public string name;
+//        public string surname;
+//        public int age;
+
+//        public StudentInfo()
+//        {
+//            Console.WriteLine("Empty Info");
+//        }
+//        public StudentInfo(string text) : this()
+//        {
+//            Console.WriteLine(text);
+//        }
+//        public StudentInfo(string m, int n) : this(m)
+//        {
+//            Console.WriteLine(n);
+//        }
+//    }
+//}
+
+
+
+
+
+//namespace OOP_AccesModifys
+//{
+//    internal class StudentInfo
+//    {
+//        public string adress;
+//        public string email;
+
+
+//    }
+//}
+
+
+
+
+
+namespace OOP_AccesModifys
+{
+    internal class StudentInfo
+    {
+        public string adress;
+        public string email;
+
+
+        public void Info()
+        {
+            Console.WriteLine("Info Movcuddur");
+        }
+
+    }
+}
+
+
+
